135a429e202de3ae003a90a8f1b8b74b7f91b020
SVENTRIPIKAL <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-09-19 17:04:13 +0000

    Create function for computer choice

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!")
+// return rock, paper, scissors randomly
+function getComputerChoice() {
+    // anonymous function call
+    return (() => {
+        // assign number from 0-2
+        let n = Math.floor(Math.random() * 3)
+        switch (n) {
+            case 0: return "Rock"
+            case 1: return "Paper"
+            default: return "Scissors"
+        }
+    })()
+}
+
+
+console.log(getComputerChoice())
